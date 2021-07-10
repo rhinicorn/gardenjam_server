147601e81a210a9d6954034085dd886914388aa1
Maria Bruner <mariab2898@gmail.com>
2021-07-10 13:06:32 -0400

    user model

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require("sequelize")
+const db = require("./db")
+
+const User = db.define("user", {
+    username: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allosNull: false,
+    },
+})
+
+module.exports = User
