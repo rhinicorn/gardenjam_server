dba346cb6860a9b254650395fec8c04a36a69820
Rhinicorn <admin@rhinicorn.com>
2021-07-10 17:02:15 -0400

    test endpoint

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const Express = require("express");
 const app = Express();
 
+app.use('/test', (req,res) => {
+  res.send('This is a message from the test endpoint')
+});
+
 app.listen(3000, () => {
   console.log('[Server]: App is listening on 3000.');
 });
