730f11b5d84ae019efdae0fb95eca8bf2f14b0d7
Justin Myers <jmyers.sde@gmail.com>
2021-07-15 21:28:27 -0400

    Trying to get post to work

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,7 +49,7 @@ router.post("/login", async (req, res) => {
 
             if (passwordComparison) {
 
-                let token = jwt.sign({ id: loginUser.id }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 });
+                let token = jwt.sign({ id: loginUser.id }, "i_am_secret", { expiresIn: 60 * 60 * 24 });
 
                 res.status(200).json({
                     user: loginUser,
